3e91ae5e2e37989a9674ac4c6625867d978a8747
ProboxDu <proboxdu@gmail.com>
2022-03-05 13:27:26 +0800

    v0.2.1 updated

--- src/components/PDFViewer.vue
+++ src/components/PDFViewer.vue
@@ -1,5 +1,10 @@
 <template>
-  <div></div>
+  <iframe
+      width="100%"
+      height="100%"
+      :src=pdfUrl
+      id="myIframe"
+  ></iframe>
 </template>
 
 <script>
@@ -10,6 +15,7 @@ export default {
   },
   data() {
     return {
+      pdfUrl : '/static/pdfjs-dist/web/viewer.html?file=http://10.2.13.232:8000/media/pdf_upload_data/【奇安信】双尾蝎（APT-C-23）伸向巴以两国的毒针.pdf'
     }
   },
   methods: {
